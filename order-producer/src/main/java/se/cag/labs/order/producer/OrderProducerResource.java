b0bf05220c6fd2a3b5740b6db44d09a9c8af6d95
Daniel Wikman <daniel.wikman@cag.se>
2021-03-23 14:09:36 +0100

    Order Producer, simple solution.

--- order-producer/src/main/java/se/cag/labs/order/producer/OrderProducerResource.java
+++ order-producer/src/main/java/se/cag/labs/order/producer/OrderProducerResource.java
@@ -1,9 +1,11 @@
 package se.cag.labs.order.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import se.cag.labs.kafka.model.Order;
@@ -13,16 +15,26 @@ import se.cag.labs.kafka.model.Order;
 public class OrderProducerResource {
 
     private OrderProducerConfiguration configuration;
+    private KafkaTemplate<String, String> template;
 
     private ObjectMapper mapper = new ObjectMapper();
 
-    public OrderProducerResource(OrderProducerConfiguration configuration) {
+    @Autowired
+    public OrderProducerResource(OrderProducerConfiguration configuration,
+                                 KafkaTemplate<String, String> template) {
         this.configuration = configuration;
+        this.template = template;
     }
 
     @PostMapping("orders/order")
     public void produceOrder(@RequestBody Order order) {
         log.info(order.toString());
+        try {
+            String orderAsString = mapper.writeValueAsString(order);
+            template.send(configuration.getTopic(), orderAsString);
+        } catch (JsonProcessingException e) {
+            log.error("Could not serialize order");
+        }
         // Just dump order on kafka for processing.
     }
 
