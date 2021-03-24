011b4302b87ed0b531689fe9c46bbbc32ecf8809
Daniel Wikman <daniel.wikman@cag.se>
2021-03-24 20:48:55 +0100

    order processing

--- order-processing/src/main/java/se/cag/labs/order/consumer/OrderProcessorConfiguration.java
+++ order-processing/src/main/java/se/cag/labs/order/consumer/OrderProcessorConfiguration.java
@@ -1,22 +1,25 @@
 package se.cag.labs.order.consumer;
 
 import lombok.Data;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @ConfigurationProperties(prefix = "kafka")
-@EnableKafka
+@EnableKafkaStreams
 @Data
 public class OrderProcessorConfiguration {
     private String servers;
@@ -27,30 +30,21 @@ public class OrderProcessorConfiguration {
     private String applicationId;
     
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(
-                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                servers);
-        props.put(
-                ConsumerConfig.GROUP_ID_CONFIG,
-                groupId);
-        props.put(
-                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        props.put(
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        return new KafkaAdmin(configs);
     }
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String>
-    kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kStreamsConfigs() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "application"); // TODO:FIXME
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        return new KafkaStreamsConfiguration(config);
     }
 
 }
--- order-processing/src/main/java/se/cag/labs/order/consumer/OrderProcessorService.java
+++ order-processing/src/main/java/se/cag/labs/order/consumer/OrderProcessorService.java
@@ -1,9 +1,13 @@
 package se.cag.labs.order.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import se.cag.labs.kafka.model.Order;
 
 /**
  * Service that in
@@ -15,14 +19,28 @@ import org.springframework.stereotype.Service;
 public class OrderProcessorService {
 
     private OrderProcessorConfiguration configuration;
+    private ObjectMapper mapper = new ObjectMapper();
 
     public OrderProcessorService(OrderProcessorConfiguration configuration) {
         this.configuration = configuration;
     }
 
-    @KafkaListener(topics = "${kafka.inTopic}")
-    public void handleEvent(String message, Acknowledgment ack) {
-        log.info("Consuming: " + message);
-        ack.acknowledge();
+    @Bean
+    public KStream<Object,Object>[] kStream(StreamsBuilder streamsBuilder) {
+        KStream<Object, Object>[] branches = streamsBuilder.stream(configuration.getInTopic())
+                .branch((k, v) -> inStock((String)v), (k, v) -> ! inStock((String) v));
+        branches[0].to(configuration.getPackTopic());
+        branches[1].to(configuration.getBackorderTopic());
+        return branches;
     }
+
+    private boolean inStock(String value) {
+        try {
+            Order order = mapper.readValue(value, Order.class);
+            return order.getLines().stream().allMatch(l -> l.getSku().startsWith("I"));
+        } catch (JsonProcessingException e) {
+            return false;
+        }
+    }
+
 }
